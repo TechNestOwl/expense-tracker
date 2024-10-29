3127e901146299f4587362e38641b83ad9fa4126
Guilherme Guimaraes <Technestowl@gmail.com>
2024-10-29 07:52:35 -0400

    added list of javascript objectives for today

---
+++ script.js
@@ -0,0 +1,12 @@
+// Data Entry:
+// I need to capture input values when the user submits the form.
+// Validate entries
+
+// Expense Object:
+// Create an object or array to store each expense (e.g., amount, category, description, date).
+
+// Local Storage:
+// Save expenses to localStorage so data persists even if the page is refreshed.
+// Retrieve and display stored data on page load.
+
+
